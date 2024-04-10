68037642a82470f9686b66bd7a9c22ac0922a687
Serg <freekey@list.ru>
2024-04-10 22:40:59 +0400

    initial commit

---
+++ data/init.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA access_schema
+    CREATE TABLE users
+    (
+        id				SERIAL		    NOT NULL	PRIMARY KEY,
+        login			VARCHAR(30)     NOT NULL    UNIQUE,
+        password		VARCHAR(30)     NOT NULL,
+        bucket			VARCHAR(255)	NOT NULL
+    );
+
+
